468eda8860182fa941d5629f379e25c1576ec361
networker002 <networkuser69t@gmail.com>
2026-04-04 17:57:23 +0300

    edited group cmd

--- src/bot/handlers/show_c.py
+++ src/bot/handlers/show_c.py
@@ -1,7 +1,8 @@
 # Telegram handler which sening list of comands and inline btn
 
-from aiogram import Router, types
+from aiogram import Router, types, F
 from aiogram.filters import Command
+from aiogram.types import LinkPreviewOptions
 from utils.command_list import get_command_list_text as get
 from utils import keyboards
 
@@ -15,13 +16,15 @@ async def cmd_show_commands(message: types.Message):
     #     parse_mode="HTML"
     # )
     commands_list = get()
-    text = f'''<b>BoostBot | Commands\nВот список команд, <b>который только пополняется!</b>
-{commands_list}'''
+    text = f'''<b>BoostBot | Commands\nВот список команд, который только пополняется!</b>
+{commands_list}
 
-    await message.answer(text, parse_mode="HTML")
+<tg-emoji emoji-id="5767420838282267710">🎨</tg-emoji> Но лучше всего пользоваться нашим <a href="t.me/mietcbot/webapp">приложением</a>'''
+
+    await message.answer(text, parse_mode="HTML", link_preview_options=LinkPreviewOptions(url="t.me/mietcbot/webapp", show_above_text=False))
 
 
-@router.callback_query(lambda c: c.data == "show_commands")
+@router.callback_query(F.data == "show_commands")
 async def on_show_commands(callback: types.CallbackQuery):
     # await callback.message.answer_document(
     #     document=types.FSInputFile(path="src/shared/fr/list.html"),
@@ -29,12 +32,15 @@ async def on_show_commands(callback: types.CallbackQuery):
     #     parse_mode="HTML",
     #     reply_markup=keyboards.get_back_commands_kb()
     # )
-    commands_lst = "\n".join(get())
+    commands_lst = get()
     await callback.message.edit_text(
-        text=f'''<b>BoostBot | Commands\nВот список команд, <b>который только пополняется!</b>
-{commands_lst}''',
+        text=f'''<b>BoostBot | Commands\nВот список команд, который только пополняется!</b>
+{commands_lst}
+
+<tg-emoji emoji-id="5767420838282267710">🎨</tg-emoji> Но лучше всего пользоваться нашим <a href="t.me/mietcbot/webapp">приложением</a>''',
         parse_mode="HTML",
-        reply_markup=keyboards.get_back_commands_kb()
+        reply_markup=keyboards.get_back_commands_kb(),
+        link_preview_options=LinkPreviewOptions(url="t.me/mietcbot/webapp", show_above_text=False)
     )
     await callback.answer()
 
