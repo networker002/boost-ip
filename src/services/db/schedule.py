e5eaeb0bd760f512aeac5f5a56bbb980aaf289ae
networker002 <networkuser69t@gmail.com>
2026-05-05 00:07:29 +0300

    Add GitHub Actions workflow for schedule checking and update group handling logic

--- src/services/db/schedule.py
+++ src/services/db/schedule.py
@@ -3,6 +3,7 @@ import requests as req
 from collections import defaultdict
 from typing import Dict, List, Any, TypedDict, Optional, Tuple
 from services import get_weeks
+from services.db.client import supabase
 import random
 from pathlib import Path
 import asyncio
@@ -57,12 +58,22 @@ class Schedule():
         # url_example = "https://www.miet.ru/schedule/data?group=%D0%98%D0%A1-25-14%D0%9E"
         self.url += self.group_name.upper()
 
-        res = req.get(url=self.url)
+
+        # res = req.get(url=self.url)
         
-        if res.status_code == 200:
-            data: list = res.json()["Data"]
-            return data
-        return []   
+        # if res.status_code == 200:
+        #     data: list = res.json()["Data"]
+        #     return data
+        # return []  
+    
+
+        res = supabase.table("schedule_updates").select("*").eq("group_name", self.group_name).execute()
+        print(res)
+        if res.data:
+            data = res.data[0]
+            last_checked = data.get("last_checked")
+            content = data.get("content")
+            return content.get("Data", [])
 
     async def get_schedule_async(self) -> Tuple[str, Dict[str, Any]]:
         timings, subjects = await asyncio.to_thread(
@@ -119,6 +130,67 @@ class Schedule():
 
         
         return get_weeks.group_now_week(result)
+    
+    def get_two_weeks(self):
+        timings = self.set_Time()
+        subjects = self.parse_by_group()
+
+        if not timings or not subjects:
+            return {}, {}, {}, {}
+
+        days = defaultdict(lambda: defaultdict(list))
+        day_names = {
+            1: 'Понедельник', 
+            2: 'Вторник', 
+            3: 'Среда', 
+            4: 'Четверг',
+            5: 'Пятницa'
+        }
+        div_days = {
+            0: "1 числитель",
+            1: "1 знаменатель",
+            2: "2 числитель",
+            3: "2 знаменатель"
+        }
+
+        for lesson in subjects:
+            day = lesson["Day"]
+            subgroup = div_days.get(lesson["DayNumber"])
+            time_info = lesson["Time"]
+            time_idx = time_info["Time"]
+            time_name = timings[time_idx - 1]["Time"] if 0 < time_idx <= len(timings) else "0 пара"
+            
+            lesson_data = {
+                'time': time_name,
+                'time_code': time_idx,
+                'subject': lesson["Class"]["Name"],
+                'teacher': lesson["Class"]["Teacher"],
+                'room': lesson["Room"]["Name"] or "Не указана"
+            }
+            
+            days[day][subgroup].append(lesson_data)
+
+        
+        result = {}
+        for day_num in sorted(days.keys()):
+            day_schedule = {}
+            for subgroup in sorted(days[day_num].keys()):
+                day_schedule[subgroup] = sorted(
+                    days[day_num][subgroup], 
+                    key=lambda x: x['time_code']
+                )
+            result[day_num] = {
+                'name': day_names.get(day_num, f'День {day_num}'),
+                'divided': day_schedule
+            }
+        # 0 -- 1 числитель
+        # 1 -- 1 знаменатель
+        # 2 -- 2 числитель
+        # 3 -- 2 знаменатель
+
+        #result[week_day][[day_name]|[data[0|1|2|3]]]
+
+        return get_weeks.group_now_week(result, previos=True)
 
 
     def run_(self) -> Tuple[str, Dict] | Any:
@@ -182,3 +254,5 @@ class Schedule():
         #result[week_day][[day_name]|[data[0|1|2|3]]]
 
         return get_weeks.group_now_week(result)
+    
+# print(Schedule("ИС-25-14О").get_two_weeks())
