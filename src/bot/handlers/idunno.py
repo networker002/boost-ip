ea2defeb86ca2bf2bc0dbbc07c7a1f4e6548f53a
networker002 <networkuser69t@gmail.com>
2026-05-07 12:33:30 +0300

    Update idunno.py

--- src/bot/handlers/idunno.py
+++ src/bot/handlers/idunno.py
@@ -1,25 +1,22 @@
 import asyncio
-
-from aiogram import Bot, types, Router, F
+from aiogram import Bot, Router, F
+from aiogram.types import Message
 from aiogram.filters import BaseFilter, StateFilter
-from utils.command_list import get_command_list_text as get
 from aiogram.enums import ChatAction
 
-router = Router()
+from utils.command_list import get_command_list_text as get
 
-c_list = get().split("\n")
+router = Router()
+c_list = [cmd.strip().lower() for cmd in get().split("\n") if cmd.strip()]
 
 class IdontKnowFilter(BaseFilter):
-    key = "idontknow"
-
-    async def __call__(self, message: types.Message) -> bool:
-        if message.text and message.text.strip().lower() not in c_list:
-            return True
-        return not not message.text
+    
+    async def __call__(self, message: Message) -> bool:
+        return bool(message.text) and message.text.strip().lower() not in c_list
 
 @router.message(IdontKnowFilter(), StateFilter(None))
-async def idunno(message: types.Message):
-    await message.bot.send_chat_action(chat_id=message.chat.id, action=ChatAction.TYPING)
+async def idunno(message: Message, bot: Bot):
+    await bot.send_chat_action(chat_id=message.chat.id, action=ChatAction.TYPING)
     await asyncio.sleep(0.5)
     await message.reply(
         "<b>Извините, я не понимаю эту команду</b>\n\n"
@@ -28,5 +25,5 @@ async def idunno(message: types.Message):
     )
 
 @router.message(F.sticker)
-async def and_st(message: types.Message):
+async def and_st(message: Message):
     await message.answer_sticker(sticker="CAACAgEAAxkBAAEDxAxp_FYHmnT8vA30q4yGskiSGiUkPQAC5ggAAjxi4Uc4BD6jxvlNxTsE")
